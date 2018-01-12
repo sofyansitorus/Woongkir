49b5b34f9b8a222909a602c1b0d4277791c2da04
Sofyan Sitorus <sofyansitorus@gmail.com>
2018-01-12 16:17:34 +0700

    Tweak account validation function

--- includes/class-woongkir.php
+++ includes/class-woongkir.php
@@ -329,7 +329,7 @@ class Woongkir extends WC_Shipping_Method {
 			if ( empty( $value ) ) {
 				throw new Exception( __( 'API Key is required.', 'woongkir' ) );
 			}
-			$account_type = $this->posted_field_value( 'account_type' );
+			$account_type = $this->validate_account_type_field( 'account_type', $this->posted_field_value( 'account_type' ) );
 			if ( $value !== $this->api_key || ( $account_type && $account_type !== $this->account_type ) ) {
 				$this->api->set_option( 'api_key', $value );
 				$this->api->set_option( 'account_type', $account_type );
