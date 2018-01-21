b5ee132030b23cc109fd161848b3dd4f88eec425
Sofyan Sitorus <sofyansitorus@gmail.com>
2018-01-21 07:27:59 +0700

    Return early if WooCommerce is inactive

--- woongkir.php
+++ woongkir.php
@@ -49,95 +49,94 @@ add_action( 'plugins_loaded', 'woongkir_load_textdomain' );
 /**
  * Check if WooCommerce is active
  */
-if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ), true ) ) {
-	/**
-	 * Wrap and load main class to ensure the classes need to extend is exist.
-	 *
-	 * @since 1.0.0
-	 */
-	function woongkir_load_dependencies() {
-		require_once WOONGKIR_PATH . 'includes/class-raja-ongkir.php';
-		require_once WOONGKIR_PATH . 'includes/class-woongkir.php';
-	}
-	add_action( 'woocommerce_shipping_init', 'woongkir_load_dependencies' );
+if ( ! in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ), true ) ) {
+	return;
+}// End if().
 
-	/**
-	 * Add plugin action links.
-	 *
-	 * Add a link to the settings page on the plugins.php page.
-	 *
-	 * @since 1.1.3
-	 *
-	 * @param  array $links List of existing plugin action links.
-	 * @return array         List of modified plugin action links.
-	 */
-	function woongkir_plugin_action_links( $links ) {
-		$links = array_merge(
-			array(
-				'<a href="' . esc_url( wp_nonce_url( admin_url( 'admin.php?page=wc-settings&tab=shipping&zone_id=0&woongkir_settings=1' ), 'woongkir_settings' ) ) . '">' . __( 'Settings', 'woongkir' ) . '</a>',
-			), $links
-		);
-
-		return $links;
-	}
-	add_action( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'woongkir_plugin_action_links' );
-
-
-	/**
-	 * Register shipping method to WooCommerce.
-	 *
-	 * @since 1.0.0
-	 * @param array $methods registered shipping methods.
-	 */
-	function woongkir_shipping_methods( $methods ) {
-		if ( class_exists( 'Woongkir' ) ) {
-			$methods['woongkir'] = 'Woongkir';
-		}
-		return $methods;
-	}
-	add_filter( 'woocommerce_shipping_methods', 'woongkir_shipping_methods' );
-
-	/**
-	 * Enqueue scripts.
-	 *
-	 * @since 1.0.0
-	 * @param string $hook Passed screen ID in admin area.
-	 */
-	function woongkir_enqueue_scripts( $hook = null ) {
-		if ( ( is_admin() && 'woocommerce_page_wc-settings' === $hook ) || ! is_admin() ) {
-			wp_register_script( 'store.js', WOONGKIR_URL . 'assets/js/store.min.js' );
-			wp_enqueue_script( 'woongkir', WOONGKIR_URL . 'assets/js/woongkir.min.js', array( 'jquery', 'store.js' ), '', true );
-			wp_localize_script(
-				'woongkir', 'woongkir_params', array(
-					'ajax_url'      => admin_url( 'ajax.php' ),
-					'json'          => array(
-						'country_url'     => add_query_arg( array( 't' => current_time( 'timestamp' ) ), WOONGKIR_URL . 'data/country.json' ),
-						'country_key'     => 'woongkir_country_data',
-						'province_url'    => add_query_arg( array( 't' => current_time( 'timestamp' ) ), WOONGKIR_URL . 'data/province.json' ),
-						'province_key'    => 'woongkir_province_data',
-						'city_url'        => add_query_arg( array( 't' => current_time( 'timestamp' ) ), WOONGKIR_URL . 'data/city.json' ),
-						'city_key'        => 'woongkir_city_data',
-						'subdistrict_url' => add_query_arg( array( 't' => current_time( 'timestamp' ) ), WOONGKIR_URL . 'data/subdistrict.json' ),
-						'subdistrict_key' => 'woongkir_subdistrict_data',
-					),
-					'text'          => array(
-						'select_country'     => __( 'Select country', 'woongkir' ),
-						'select_province'    => __( 'Select province', 'woongkir' ),
-						'select_city'        => __( 'Select city', 'woongkir' ),
-						'select_subdistrict' => __( 'Select subdistrict', 'woongkir' ),
-					),
-					'debug'         => ( 'yes' === get_option( 'woocommerce_shipping_debug_mode', 'no' ) ),
-					'show_settings' => ( isset( $_GET['woongkir_settings'] ) && wp_verify_nonce( $_GET['_wpnonce'], 'woongkir_settings' ) && is_admin() ),
-				)
-			);
-		}
-	}
-	add_action( 'admin_enqueue_scripts', 'woongkir_enqueue_scripts', 999 );
-	add_action( 'wp_enqueue_scripts', 'woongkir_enqueue_scripts', 999 );
-
-	// Show city field in the shipping calculator form.
-	add_filter( 'woocommerce_shipping_calculator_enable_city', '__return_true' );
-
+/**
+ * Wrap and load main class to ensure the classes need to extend is exist.
+ *
+ * @since 1.0.0
+ */
+function woongkir_load_dependencies() {
+	require_once WOONGKIR_PATH . 'includes/class-raja-ongkir.php';
+	require_once WOONGKIR_PATH . 'includes/class-woongkir.php';
 }
+add_action( 'woocommerce_shipping_init', 'woongkir_load_dependencies' );
+
+/**
+ * Add plugin action links.
+ *
+ * Add a link to the settings page on the plugins.php page.
+ *
+ * @since 1.1.3
+ *
+ * @param  array $links List of existing plugin action links.
+ * @return array         List of modified plugin action links.
+ */
+function woongkir_plugin_action_links( $links ) {
+	$links = array_merge(
+		array(
+			'<a href="' . esc_url( wp_nonce_url( admin_url( 'admin.php?page=wc-settings&tab=shipping&zone_id=0&woongkir_settings=1' ), 'woongkir_settings' ) ) . '">' . __( 'Settings', 'woongkir' ) . '</a>',
+		), $links
+	);
+
+	return $links;
+}
+add_action( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'woongkir_plugin_action_links' );
 
 
+/**
+ * Register shipping method to WooCommerce.
+ *
+ * @since 1.0.0
+ * @param array $methods registered shipping methods.
+ */
+function woongkir_shipping_methods( $methods ) {
+	if ( class_exists( 'Woongkir' ) ) {
+		$methods['woongkir'] = 'Woongkir';
+	}
+	return $methods;
+}
+add_filter( 'woocommerce_shipping_methods', 'woongkir_shipping_methods' );
+
+/**
+ * Enqueue scripts.
+ *
+ * @since 1.0.0
+ * @param string $hook Passed screen ID in admin area.
+ */
+function woongkir_enqueue_scripts( $hook = null ) {
+	if ( ( is_admin() && 'woocommerce_page_wc-settings' === $hook ) || ! is_admin() ) {
+		wp_register_script( 'store.js', WOONGKIR_URL . 'assets/js/store.min.js' );
+		wp_enqueue_script( 'woongkir', WOONGKIR_URL . 'assets/js/woongkir.min.js', array( 'jquery', 'store.js' ), '', true );
+		wp_localize_script(
+			'woongkir', 'woongkir_params', array(
+				'ajax_url'      => admin_url( 'ajax.php' ),
+				'json'          => array(
+					'country_url'     => add_query_arg( array( 't' => current_time( 'timestamp' ) ), WOONGKIR_URL . 'data/country.json' ),
+					'country_key'     => 'woongkir_country_data',
+					'province_url'    => add_query_arg( array( 't' => current_time( 'timestamp' ) ), WOONGKIR_URL . 'data/province.json' ),
+					'province_key'    => 'woongkir_province_data',
+					'city_url'        => add_query_arg( array( 't' => current_time( 'timestamp' ) ), WOONGKIR_URL . 'data/city.json' ),
+					'city_key'        => 'woongkir_city_data',
+					'subdistrict_url' => add_query_arg( array( 't' => current_time( 'timestamp' ) ), WOONGKIR_URL . 'data/subdistrict.json' ),
+					'subdistrict_key' => 'woongkir_subdistrict_data',
+				),
+				'text'          => array(
+					'select_country'     => __( 'Select country', 'woongkir' ),
+					'select_province'    => __( 'Select province', 'woongkir' ),
+					'select_city'        => __( 'Select city', 'woongkir' ),
+					'select_subdistrict' => __( 'Select subdistrict', 'woongkir' ),
+				),
+				'debug'         => ( 'yes' === get_option( 'woocommerce_shipping_debug_mode', 'no' ) ),
+				'show_settings' => ( isset( $_GET['woongkir_settings'] ) && wp_verify_nonce( $_GET['_wpnonce'], 'woongkir_settings' ) && is_admin() ),
+			)
+		);
+	}
+}
+add_action( 'admin_enqueue_scripts', 'woongkir_enqueue_scripts', 999 );
+add_action( 'wp_enqueue_scripts', 'woongkir_enqueue_scripts', 999 );
+
+// Show city field in the shipping calculator form.
+add_filter( 'woocommerce_shipping_calculator_enable_city', '__return_true' );
