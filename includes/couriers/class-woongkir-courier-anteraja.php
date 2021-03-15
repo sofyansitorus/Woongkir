84b35c632a0b4863880d47fc56a880a35edc3d86
Sofyan Sitorus <sofyansitorus@gmail.com>
2021-03-15 12:43:56 +0700

    Added new courierL AnterAja

---
+++ includes/couriers/class-woongkir-courier-anteraja.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * The file that defines the Woongkir_Courier_ANTERAJA class
+ *
+ * @link       https://github.com/sofyansitorus
+ * @since      1.2.12
+ *
+ * @package    Woongkir
+ * @subpackage Woongkir/includes
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * The Woongkir_Courier_ANTERAJA class.
+ *
+ * @since      1.2.12
+ * @package    Woongkir
+ * @subpackage Woongkir/includes
+ * @author     Sofyan Sitorus <sofyansitorus@gmail.com>
+ */
+class Woongkir_Courier_ANTERAJA extends Woongkir_Courier {
+
+	/**
+	 * Courier Code
+	 *
+	 * @since 1.2.12
+	 *
+	 * @var string
+	 */
+	public $code = 'anteraja';
+
+	/**
+	 * Courier Label
+	 *
+	 * @since 1.2.12
+	 *
+	 * @var string
+	 */
+	public $label = 'AnterAja';
+
+	/**
+	 * Courier Website
+	 *
+	 * @since 1.2.12
+	 *
+	 * @var string
+	 */
+	public $website = 'http://anteraja.id';
+
+	/**
+	 * Get courier services for domestic shipping
+	 *
+	 * @since 1.2.12
+	 *
+	 * @return array
+	 */
+	public function get_services_domestic_default() {
+		return array(
+			'ND'  => 'Next Day',
+			'REG' => 'Regular',
+			'SD'  => 'Same Day',
+		);
+	}
+
+	/**
+	 * Get courier account for domestic shipping
+	 *
+	 * @since 1.2.12
+	 *
+	 * @return array
+	 */
+	public function get_account_domestic() {
+		return array(
+			'pro',
+		);
+	}
+}
