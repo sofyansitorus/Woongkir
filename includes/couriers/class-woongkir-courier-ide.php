f62b11b080c6f96f76fa737a9a5e009a493ec8ed
Sofyan Sitorus <sofyansitorus@gmail.com>
2022-06-05 20:04:02 +0700

    Added new courier: IDexpress Service Solution (IDE)

---
+++ includes/couriers/class-woongkir-courier-ide.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * The file that defines the Woongkir_Courier_IDE class
+ *
+ * @link       https://github.com/sofyansitorus
+ * @since      ??
+ *
+ * @package    Woongkir
+ * @subpackage Woongkir/includes
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * The Woongkir_Courier_IDE class.
+ *
+ * @since      ??
+ * @package    Woongkir
+ * @subpackage Woongkir/includes
+ * @author     Sofyan Sitorus <sofyansitorus@gmail.com>
+ */
+class Woongkir_Courier_IDE extends Woongkir_Courier {
+
+	/**
+	 * Courier Code
+	 *
+	 * @since ??
+	 *
+	 * @var string
+	 */
+	public $code = 'ide';
+
+	/**
+	 * Courier Label
+	 *
+	 * @since ??
+	 *
+	 * @var string
+	 */
+	public $label = 'IDexpress Service Solution';
+
+	/**
+	 * Courier Website
+	 *
+	 * @since ??
+	 *
+	 * @var string
+	 */
+	public $website = 'http://idexpress.com';
+
+	/**
+	 * Get courier services for domestic shipping
+	 *
+	 * @since ??
+	 *
+	 * @return array
+	 */
+	public function get_services_domestic_default() {
+		return array(
+			'STD' => 'Standard Service',
+		);
+	}
+
+	/**
+	 * Get courier account for domestic shipping
+	 *
+	 * @since ??
+	 *
+	 * @return array
+	 */
+	public function get_account_domestic() {
+		return array(
+			'pro',
+		);
+	}
+}
